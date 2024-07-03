6b42d808787b3c07aa2701accbe395bcfe392835
joshua468 <adegbesanjoshua9@gmail.com>
2024-07-03 17:20:18 +0100

    Add basic web server and API handler

--- api/handler.go
+++ api/handler.go
@@ -9,50 +9,50 @@ import (
 )
 
 type Response struct {
-    ClientIP  string `json:"client_ip"`
-    Location  string `json:"location"`
-    Greeting  string `json:"greeting"`
+	ClientIP string `json:"client_ip"`
+	Location string `json:"location"`
+	Greeting string `json:"greeting"`
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    visitorName := r.URL.Query().Get("visitor_name")
-    if visitorName == "" {
-        visitorName = "Visitor"
-    }
+	visitorName := r.URL.Query().Get("visitor_name")
+	if visitorName == "" {
+		visitorName = "Visitor"
+	}
 
-    clientIP := getClientIP(r)
+	clientIP := getClientIP(r)
 
-    city := "New York"
-    temperature := 11.0
+	city := "New York"
+	temperature := 11.0
 
-    greeting := fmt.Sprintf("Hello, %s! The temperature is %.1f degrees Celsius in %s", visitorName, temperature, city)
-    response := Response{
-        ClientIP: clientIP,
-        Location: city,
-        Greeting: greeting,
-    }
+	greeting := fmt.Sprintf("Hello, %s! The temperature is %.1f degrees Celsius in %s", visitorName, temperature, city)
+	response := Response{
+		ClientIP: clientIP,
+		Location: city,
+		Greeting: greeting,
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(response)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
 
 func getClientIP(r *http.Request) string {
-    headersToCheck := []string{"X-Forwarded-For", "X-Real-IP"}
+	headersToCheck := []string{"X-Forwarded-For", "X-Real-IP"}
 
-    for _, header := range headersToCheck {
-        if ip := r.Header.Get(header); ip != "" {
-            if header == "X-Forwarded-For" {
-                ips := strings.Split(ip, ",")
-                return strings.TrimSpace(ips[0])
-            }
-            return ip
-        }
-    }
+	for _, header := range headersToCheck {
+		if ip := r.Header.Get(header); ip != "" {
+			if header == "X-Forwarded-For" {
+				ips := strings.Split(ip, ",")
+				return strings.TrimSpace(ips[0])
+			}
+			return ip
+		}
+	}
 
-    ip, _, err := net.SplitHostPort(r.RemoteAddr)
-    if err != nil {
-        return "unknown"
-    }
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "unknown"
+	}
 
-    return ip
+	return ip
 }
